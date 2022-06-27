4ce57d3c2b17de831c4bec9f0b5c390321d16db2
Caroline <alyseflorez@hotmail.fr>
2022-06-27 15:29:49 +0200

    exo objects et functions

---
+++ index.js
@@ -0,0 +1,55 @@
+
+// 01
+// const cat = {
+//     name : "Garfield",
+//     age: 3,
+//     isCute: true   
+// }
+// console.log(cat.age)
+
+// if (cat.isCute) {
+//     console.log("So cute")
+// }
+
+
+// 02
+// const cat2 = {
+//     name: "orange",
+//     age: "13",
+//     isCute: false
+// }
+// let cats = [cat2, cat]
+
+// console.log(cats[1].age)
+// console.log(cats[0].isCute)
+
+
+// 03
+// const checkIEven = (x) => {
+//     const num = x % 2
+//     console.log(num)
+//     if (num === 0) {
+//         console.log("Even")
+//     } else {
+//         console.log("Odd")
+//     }
+//     console.log(num)
+// }
+// checkIEven(5)
+
+
+// 04
+// const compare = (x, y) => {
+//     const nb = x > y
+
+//     if (x > y) {
+//         console.log("X is bigger")
+//     } else {
+//         console.log("Y is bigger")
+//     }
+//     console.log(nb)
+// }
+// compare(14, 9)
+
+
+// 05
